2ac432af18a3b290da023bf07fcea4ced057c249
AndreaTrento95 <trentora95@gmail.com>
2021-05-06 15:36:44 +0200

    svolgimento

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+
+    el: '#app',
+    data:{
+        alertActive: false,
+        strTodo:'',
+        todos: [
+            {
+                todo: 'Comprare una macchina',
+                checked: false
+            },
+            {
+                todo: 'Guidare la macchina',
+                checked: false
+            },
+            {
+                todo: 'Andare in pringione',
+                checked: true
+            },
+            {
+                todo: 'Farsi pagare la cauzione',
+                checked: false
+            },
+        ]
+    },
+    methods:{
+        removeTodo(index){
+            this.todos.splice(index, 1);
+        }
+    },
+    addTodo(){
+        if(this.strTodo.length > 2){
+            this.todos.push({todo: this.strTodo, checked: false});
+            this.strTodo = '';
+        }else{
+            this.alertActive = true;
+            setTimeout(() => {
+                this.alertActive = false;
+            }, 2000);
+        }
+    },
+
+});
