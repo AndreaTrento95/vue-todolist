a058dfcca4e372c3808dbe264b1ec752786da43f
AndreaTrento95 <trentora95@gmail.com>
2021-05-07 09:35:05 +0200

    svolgimento

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,18 +26,19 @@ const app = new Vue({
     methods:{
         removeTodo(index){
             this.todos.splice(index, 1);
-        }
+        },
+        addTodo(){
+            if(this.strTodo.length > 2){
+                this.todos.push({todo: this.strTodo, checked: false});
+                this.strTodo = '';
+            }else{
+                this.alertActive = true;
+                setTimeout(() => {
+                    this.alertActive = false;
+                }, 2000);
+            }
+        },
     },
-    addTodo(){
-        if(this.strTodo.length > 2){
-            this.todos.push({todo: this.strTodo, checked: false});
-            this.strTodo = '';
-        }else{
-            this.alertActive = true;
-            setTimeout(() => {
-                this.alertActive = false;
-            }, 2000);
-        }
-    },
+    
 
 });
